73fb3538da5efa5d62700b08f377c708b302efbd
XDDudeGuy <60826762+XDDudeGuy@users.noreply.github.com>
2024-10-05 00:12:58 -0400

    Refactored safe_math.rs

--- src/safe_operations.rs
+++ src/safe_operations.rs
@@ -49,8 +49,7 @@ macro_rules! safe_math {
                         }
                     }
                 }
-            }
-            else {
+            } else {
                 Result::Err(returned_value)
             }
         }
